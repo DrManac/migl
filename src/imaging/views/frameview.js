c2ceafdca85b3eef8446337d7a0461ab515fc0a7
manac <drploskikh@gmail.com>
2017-06-17 12:44:44 +0300

    voi window support

--- src/imaging/views/frameview.js
+++ src/imaging/views/frameview.js
@@ -22,6 +22,31 @@ class FrameView extends ViewBase {
 			this._hasChanges3d = true;
 		});
 	}
+	get invert() { return this._el3d._black > this._el3d._white; }
+	set invert(invert) {
+		if(invert == this.invert) return;
+		var tmp = this._el3d._black;
+		this._el3d._black = this._el3d._white;
+		this._el3d._white = tmp;
+		this._hasChanges3d = true;
+	}
+	get voi() { return this.invert ?
+		{ black: this._el3d._black, white: this._el3d._white } :
+		{ black: this._el3d._white, white: this._el3d._black };
+	}
+	set voi(voi) {
+		if(voi.black > voi.white - 1)
+			voi.black = voi.white - 1;
+		if(this.invert)
+		{
+			this._el3d._black = voi.white;
+			this._el3d._white = voi.black;
+		} else {
+			this._el3d._black = voi.black;
+			this._el3d._white = voi.white;
+		}
+		this._hasChanges3d = true;
+	}
 	_updateProjection() {
 		var aspect = this.Width / this.Height;
 		if(aspect > 1)
--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 //import dicomParser from 'dicom-parser';
-//import { mat4, mat3, mat2d, vec3, vec2 } from 'gl-matrix';
-//import twgl from 'twgl-base.js'
-//import {ImageInfo, Image} from './imaging/image.js'
-//import {VolumeInfo, Volume} from './imaging/volume.js'
 
 export {ImageInfo, Image} from './imaging/image.js'
 export {VolumeInfo, Volume} from './imaging/volume.js'
