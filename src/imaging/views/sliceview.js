70fa37f790752f70b48f8065743f48ca6a114958
manac <drploskikh@gmail.com>
2017-08-01 16:06:33 +0300

    orts transform

--- src/imaging/views/sliceview.js
+++ src/imaging/views/sliceview.js
@@ -9,6 +9,9 @@ class SliceView extends VolumeViewBase {
 		this._el3d = new Slice();
 		this.Add3dSceneElement(this._el3d);
 		this.SetLut(Luts.list[0]);
+		this._xort = [1, 0, 0];
+		this._yort = [0, 1, 0];
+		this._zort = [0, 0, 1];
 	}
 	SetLut(lut) {
 		lut.image.then((img) => {
@@ -17,25 +20,38 @@ class SliceView extends VolumeViewBase {
 		});
 	}
 	SetOrts(xort, yort, zort) {
-		this._el3d.world = mat4.fromValues(
-			xort[0], xort[1], xort[2], 0,
-			yort[0], yort[1], yort[2], 0,
-			zort[0], zort[1], zort[2], 0,
-			0, 0, 0, 1);
+		this._xort = xort;
+		this._yort = yort;
+		this._zort = zort;
+		this._onWindowResize();
 	}
-	_updateProjection() {
+	_updateTransforms() {
 		var aspect = this.Width / this.Height;
+		var amtx = mat4.create();
+		var amtxinv = mat4.create();
 		if(aspect > 1)
-			mat4.ortho(this._el3d.projection, -aspect, aspect, 1, -1, -1, 1);
+		{
+			mat4.fromScaling(amtx, [1 / aspect, 1, 1]);
+			mat4.fromScaling(amtxinv, [aspect, 1, 1]);
+		}
 		else
-			mat4.ortho(this._el3d.projection, -1, 1, 1 / aspect, -1 / aspect, -1, 1);
-		//mat4.ortho(this._el3d.projection, -1, 1, 1, -1, -1, 1);
-		mat4.scale(this._el3d.projection, this._el3d.projection, [.5, .5, .5]);
-		mat4.fromXRotation(this._el3d.view, 0.78);
-		mat4.rotateZ(this._el3d.view, this._el3d.view, 0.78);
+		{
+			mat4.fromScaling(amtx, [1, aspect, 1]);
+			mat4.fromScaling(amtxinv, [1, 1 / aspect, 1]);
+		}
+		mat4.ortho(this._el3d.projection, -1, 1, 1, -1, -1, 1);
+		mat4.mul(this._el3d.projection, this._el3d.projection, amtx);
+
+		var ori = mat4.fromValues(
+			...this._xort, 0,
+			...this._yort, 0,
+			...this._zort, 0,
+			0, 0, 0, 1);
+		mat4.mul(this._el3d.world, ori, amtxinv);
+		mat4.invert(this._el3d.view, ori);
 	}
 	_onWindowResize() {
-		this._updateProjection();
+		this._updateTransforms();
 		super._onWindowResize();
 	}
 }
